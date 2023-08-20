406a95d2693052e722ef32ba9561b00c575938cb
d-floe <dfloe.art@gmail.com>
2023-08-20 13:24:25 -0500

    global navigation time

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,10 +21,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Search(),
     Component.Darkmode(),
     Component.GlobalNav(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.DesktopOnly(Component.TableOfContents())
   ],
-  right: [Component.Graph(), Component.Backlinks()],
+  right: [Component.DesktopOnly(Component.TableOfContents()),Component.Graph(), Component.Backlinks()],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
@@ -32,9 +30,9 @@ export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.GlobalNav()
   ],
   right: [],
 }
